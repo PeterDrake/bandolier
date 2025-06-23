153bdd17f8fa6b0e4fc057eabae9952532251a1f
Peter Drake <drake@lclark.edu>
2025-06-23 16:22:40 -0700

    Window title is now Bandolier.

--- bandolier.py
+++ bandolier.py
@@ -1,4 +1,4 @@
-# From https://doc.qt.io/qtforpython-6/gettingstarted.html#getting-started
+# Adapted from https://doc.qt.io/qtforpython-6/gettingstarted.html#getting-started
 import sys
 import random
 from PySide6 import QtCore, QtWidgets
@@ -6,18 +6,15 @@ from PySide6 import QtCore, QtWidgets
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
         self.button = QtWidgets.QPushButton("Click me!")
         self.text = QtWidgets.QLabel("Hello World",
                                      alignment=QtCore.Qt.AlignCenter)
-
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
-
         self.button.clicked.connect(self.magic)
+        self.setWindowTitle('Bandolier')
 
     @QtCore.Slot()
     def magic(self):
