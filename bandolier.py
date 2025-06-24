4c4deb4f1a45354505f54524b9c5e20c1b07b2bb
Peter Drake <drake@lclark.edu>
2025-06-23 18:22:09 -0700

    Clicking adds an x to the corresponding text area.

--- bandolier.py
+++ bandolier.py
@@ -6,31 +6,27 @@ from PySide6 import QtCore, QtWidgets
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        # self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-        # self.button = QtWidgets.QPushButton("Click me!")
-        # self.text = QtWidgets.QLabel("Hello World",
-        #                              alignment=QtCore.Qt.AlignCenter)
         self.layout = QtWidgets.QVBoxLayout(self)
         for i in range(4):
             row = QtWidgets.QGroupBox() #outer containing box for each row
             layout = QtWidgets.QHBoxLayout(row)
             button = QtWidgets.QPushButton(f'Copy {i}')
-            #button.sizePolicy().setVerticalPolicy(QtWidgets.QSizePolicy.Policy.Expanding)
             button.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Expanding)
-            text = QtWidgets.QPlainTextEdit(f'Line {i}')  #labels for each line
+            text = QtWidgets.QPlainTextEdit(f'')  #labels for each line
             layout.addWidget(button)
             layout.addWidget(text)
             row.setLayout(layout)
             self.layout.addWidget(row)
-        # self.layout.addWidget(self.text)
-        # self.layout.addWidget(self.button)
-            button.clicked.connect(self.magic)
+            button.clicked.connect(MyWidget.make_command(text))
         self.setWindowTitle('Bandolier')
 
-    @QtCore.Slot()
-    def magic(self):
-        #self.text.setText(random.choice(self.hello))    #no longer needed
-        print("Hey, I've been clicked")
+    @staticmethod
+    def make_command(text):
+        return lambda : text.setPlainText(text.toPlainText() + 'x')
+
+    # # @QtCore.Slot()
+    # def magic(self):
+    #     print("Hey, I've been clicked")
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
